b966ab469756630c17557bbb111450e8751282b4
Robert Zimmerman <robertz@colortechnology.com>
2014-11-17 15:37:52 -0800

    make route compatible with rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-if(Rails.version.split('.').first.to_i < 3)
-  ActionController::Routing::Routes.draw do |map|
-    map.open_window 'window_rails/open_window', :controller => :window_rails, :action => :open_window
+if(Rails.version.split('.').first.to_i > 2)
+  Rails.application.routes.draw do
+    match 'window_rails/open_window' => 'window_rails#open_window', :as => :open_window, via: [:get, :post]
   end
 else
-  Rails.application.routes.draw do
-    match 'window_rails/open_window' => 'window_rails#open_window', :as => :open_window
+  ActionController::Routing::Routes.draw do |map|
+    map.open_window 'window_rails/open_window', :controller => :window_rails, :action => :open_window
   end
 end
